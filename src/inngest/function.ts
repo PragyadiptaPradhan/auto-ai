4f6bbde14fcbca063ba347a59ed481493164fc6b
Pragyadipta Pradhan <168467528+PragyadiptaPradhan@users.noreply.github.com>
2025-06-30 20:39:33 +0530

    feat: integrate @inngest/agent-kit and update invoke mutation
    - Added @inngest/agent-kit dependency to package.json.
    - Updated the page component to include an input field for user input.
    - Modified the invoke mutation to accept 'value' instead of 'text'.
    - Implemented a new helloWorld function using the agent kit to process the input value.

--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,17 +1,25 @@
+import { gemini, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    // Imagine this is a download step
-    await step.sleep("wait-a-moment", "30s");
 
-    // Imageine this is a transcript step
-    await step.sleep("wait-a-moment", "10");
+     const codeAgent = createAgent({
+      name: "codeAgent",
+      system: "You are an expert next.js developer.  You write readable, maintainable code. You write simple Next.js and React Snippets.",
+      model: gemini({ model: "gemini-1.5-flash" }),
+    });
+    const { output } = await codeAgent.run(
+      `Write the following Snippet: ${event.data.value}`
+    );
+    console.log(output);
 
-    // Imagine this is a summary
-    await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.email}!` };
+
+
+
+    return { output };
   },
 );
